8b70dea22ea71ba2d136f2b3f7be12ccfdc9cc3f
Arshdeep Sandhu <Arshsarhali@gmail.com>
2022-01-28 20:21:38 -0800

    Updates

---
+++ precache-manifest.6c9473099cfe7b9938db1ddbda151d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3924f2498e58f6bf55d3d10b8848db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ef92004537bfa173a68",
+    "url": "/monsters-rolodex/static/css/main.bc4502a3.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e36c4017ec0df4e6e55",
+    "url": "/monsters-rolodex/static/js/3.7b815fc8.chunk.js"
+  },
+  {
+    "revision": "5ef92004537bfa173a68",
+    "url": "/monsters-rolodex/static/js/main.e3d4f6b0.chunk.js"
+  },
+  {
+    "revision": "8255626bc43072b12186",
+    "url": "/monsters-rolodex/static/js/runtime-main.a6cb5aee.js"
+  }
+]);
