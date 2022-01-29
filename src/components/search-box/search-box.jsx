ce40854ac3d330aa682b1544db4eed0dd0f3c325
Arshdeep Sandhu <Arshsarhali@gmail.com>
2022-01-28 20:02:59 -0800

    First React Project that allow a search over monsters

---
+++ src/components/search-box/search-box.jsx
@@ -0,0 +1,9 @@
+import './search-box.css'
+
+export const SearchBox =({placeholder, handleChange}) =>(
+    <input className='search'
+    type='search' 
+    placeholder={placeholder}
+     onChange={handleChange}
+     />
+)
